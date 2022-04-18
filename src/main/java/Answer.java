dcf25b4e951ac9ae4d86d37604a02242cd09735c
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2022-04-18 11:40:58 -0400

    added constructors that include ID's for the newly added ID system

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -1,18 +1,38 @@
 public class Answer {
+    private final String id;
     private final String question;
     private final String definition;
     private final String[] subMethods;
 
+    // CONSTRUCTORS
+    // no ID's
     public Answer(String questionText, String correctDefinition, String[] correctSubMethods) {
+        this.id = null;
         this.question = questionText;
         this.definition = correctDefinition;
         this.subMethods = correctSubMethods;
     }
     public Answer(String questionText, String correctDefinition) {
+        this.id = null;
         this.question = questionText;
         this.definition = correctDefinition;
         this.subMethods = null;
     }
+    // yes ID's
+    public Answer(String id, String questionText, String correctDefinition, String[] correctSubMethods) {
+        this.id = id;
+        this.question = questionText;
+        this.definition = correctDefinition;
+        this.subMethods = correctSubMethods;
+    }
+    public Answer(String id, String questionText, String correctDefinition) {
+        this.id = id;
+        this.question = questionText;
+        this.definition = correctDefinition;
+        this.subMethods = null;
+    }
+
+    public String getId() { return id; }
 
     public String getQuestion() {
         return question;
@@ -22,7 +42,5 @@ public class Answer {
         return definition;
     }
 
-    public String[] getSubMethods() {
-        return subMethods;
-    }
+    public String[] getSubMethods() { return subMethods; }
 }
